1b364daa9e03b0b9e06e07e0e6a972703a79344e
Romi H. <romi@getbowtied.com>
2025-12-10 10:12:24 +0200

    ...

--- index.php
+++ index.php
@@ -10,17 +10,13 @@ if (!class_exists('TW_Components')) {
         private static $instance;
         private $base_path;
         private $base_url;
-        private $context_type;
-        private $active_server_components = [];
+        private $handle_prefix;
 
         private function __construct() {
-            $this->context_type = strpos(__DIR__, 'themes') !== false ? 'theme' : 'plugin';
-            $this->setup_paths();
-            $this->register_hooks();
-        }
-
-        private function setup_paths() {
-            if ($this->context_type === 'theme') {
+            $is_theme = strpos(__DIR__, 'themes') !== false;
+            $context_type = $is_theme ? 'theme' : 'plugin';
+            
+            if ($is_theme) {
                 $this->base_path = get_template_directory();
                 $this->base_url = get_template_directory_uri();
             } else {
@@ -29,61 +25,42 @@ if (!class_exists('TW_Components')) {
                 $this->base_path = $plugin_file ? plugin_dir_path($plugin_file) : $plugin_root;
                 $this->base_url = $plugin_file ? plugin_dir_url($plugin_file) : plugins_url(basename($plugin_root));
             }
-        }
-
-        private function register_hooks() {
-            add_action('init', [$this, 'load_server_components'], 1);
-            $enqueue_hook = $this->context_type === 'theme' ? 'wp_enqueue_scripts' : 'admin_enqueue_scripts';
-            add_action($enqueue_hook, [$this, 'enqueue_assets']);
-        }
-
-        private function get_filter_name() {
-            return "tw_component_loader_active_components_{$this->context_type}";
-        }
-
-        private function get_handle() {
-            return "tw-{$this->context_type}";
-        }
-
-        private function get_active_server_components() {
-            if (empty($this->active_server_components)) {
-                $this->active_server_components = apply_filters($this->get_filter_name(), []) ?: [];
-            }
-            return $this->active_server_components;
+            
+            $this->handle_prefix = "tw-$context_type";
+            
+            add_action('init', [$this, 'load_server_components']);
+            add_action($is_theme ? 'wp_enqueue_scripts' : 'admin_enqueue_scripts', [$this, 'enqueue_assets']);
         }
 
         public function load_server_components() {
             $server_side_base = "$this->base_path/ui/src/components/app/server-side";
-            foreach ($this->get_active_server_components() as $component_slug) {
-                $component_file = "$server_side_base/$component_slug/index.php";
-                if (file_exists($component_file)) include_once $component_file;
+            if (is_dir($server_side_base)) {
+                foreach (glob("$server_side_base/*/index.php") as $file) {
+                    if (file_exists($file)) include_once $file;
+                }
             }
         }
 
         public function enqueue_assets() {
             $assets_path = "$this->base_path/ui/assets";
             $assets_url = "$this->base_url/ui/assets";
-            $styles_file = "$assets_path/styles.css";
-            $version = file_exists($styles_file) ? filemtime($styles_file) : '1.0.0';
-            $handle = $this->get_handle();
             
-            wp_enqueue_style("$handle-style", "$assets_url/styles.css", [], $version);
-            wp_enqueue_script("$handle-script", "$assets_url/scripts.js", ['wp-element'], $version, true);
-        }
-
-        public function get_context_type() {
-            return $this->context_type;
+            $styles_path = "$assets_path/styles.css";
+            $styles_url = "$assets_url/styles.css";
+            
+            $scripts_path = "$assets_path/scripts.js";
+            $scripts_url = "$assets_url/scripts.js";
+            
+            $get_version = fn($file) => file_exists($file) ? filemtime($file) : '1.0.0';
+            
+            wp_enqueue_style("$this->handle_prefix-style", $styles_url, [], $get_version($styles_path));
+            wp_enqueue_script("$this->handle_prefix-script", $scripts_url, ['wp-element'], $get_version($scripts_path), true);
         }
 
         public static function init() {
             return self::$instance ??= new self();
         }
     }
-}
-
-function tw_register_components($server_components) {
-    $instance = TW_Components::init();
-    add_filter("tw_component_loader_active_components_{$instance->get_context_type()}", fn() => $server_components);
 }
 
 TW_Components::init();
